9f77d074f9b5f1feb6ab9a66b5910df2e2b90f16
Kayimjon Mamarasulov <xmamarasulovk@stuba.sk>
2021-05-17 17:11:41 +0200

    Final

--- src/main/java/sk/stuba/fei/uim/oop/Assignment2.java
+++ src/main/java/sk/stuba/fei/uim/oop/Assignment2.java
@@ -1,10 +1,5 @@
 package sk.stuba.fei.uim.oop;
 
-import sk.stuba.fei.uim.oop.Maze.MazeClass;
-import sk.stuba.fei.uim.oop.UI.MainUI;
-
-import javax.swing.*;
-
 public class Assignment2 {
 
     public static void main(String[] args) {
